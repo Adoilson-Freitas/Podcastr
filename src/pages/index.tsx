265c2f26f7a1bef42c36683f23383ae6d4974ac3
Adoilson Freitas <adoilsonfreitas@gmail.com>
2021-04-21 13:53:06 -0300

    Consuming API in home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,20 @@
-export default function Home() {
+export default function Home(props) {
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>{props.episodes.title}</h1>
+      <p>{JSON.stringify(props.episodes.title)}</p>
+    </div>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
